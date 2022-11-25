2cfaf5c3b00d8fb37c307ffcbce4a7c1a4dc0ad7
wilsongoltara <wilson.goltara15@gmail.com>
2022-11-25 15:44:55 -0300

    feat: add shared component

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import React from "react";
+import Shared from "../Shared";
 
 const Content = () => {
   return (
-    <div className="h-screen w-full">
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row text-gray-400">
+    <section id="home">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-5/6 px-4 md:flex-row text-gray-400">
         <div className="uppercase">
           <p className="text-start ml-14">i am</p>
           <h2 className="text-9xl text-white text-center mt-0 font-black shadow-gray-900 py-2">
@@ -13,7 +12,8 @@ const Content = () => {
           <p className="text-end mr-14">a junior web developer</p>
         </div>
       </div>
-    </div>
+      <Shared />
+    </section>
   );
 };
 
