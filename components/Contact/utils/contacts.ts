73a19b3b4dbf26351d9349f535fd67aa2955344b
wilsongoltara <wilson.goltara15@gmail.com>
2022-12-08 16:47:30 -0300

    feat: add Contact section

---
+++ components/Contact/utils/contacts.ts
@@ -0,0 +1,21 @@
+export type Contact = {
+  typeContact: string;
+  contact: string;
+};
+
+const contacts: Contact[] = [
+  {
+    typeContact: "Address",
+    contact: "Brazil",
+  },
+  {
+    typeContact: "Phone",
+    contact: "+55 (27) 99663-2892",
+  },
+  {
+    typeContact: "E-mail",
+    contact: "wilson.goltara@gmail.com",
+  },
+];
+
+export default contacts;
