cda0664d13b1e33c7d0e4d5b45c0e697a46351c0
wilsongoltara <wilson.goltara15@gmail.com>
2022-12-08 20:47:03 -0300

    feat: add Projects section

--- components/Copyright/index.tsx
+++ components/Copyright/index.tsx
@@ -4,14 +4,14 @@ const Copyright = () => {
   return (
     <>
       <p className="sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
-        WG &copy; 2022
+        WG &copy; 2022 by
         <a
           href="https://github.com/goltarawilson"
           className="ml-1"
           rel="noopener noreferrer"
           target="_blank"
         >
-          Wilson S Goltara
+          Wilson S. Goltara
         </a>
       </p>
     </>
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -3,7 +3,7 @@ import Footer from "../Footer";
 const Content = () => {
   return (
     <section id="home" className="text-gray-400 body-font">
-      <div className="mx-auto flex flex-col items-center justify-center h-5/6 px-4">
+      <div className="mx-auto flex flex-col items-center justify-center h-full px-4">
         <div className="uppercase">
           <p className="text-start">i am</p>
           <h2 className="text-7xl md:text-9xl text-white text-center mt-0 font-black shadow-gray-900 py-2">
@@ -12,7 +12,6 @@ const Content = () => {
           <p className="text-end">a junior web developer</p>
         </div>
       </div>
-      <Footer />
     </section>
   );
 };
