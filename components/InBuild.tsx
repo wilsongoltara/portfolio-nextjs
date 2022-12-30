80960efa54962ca1a3251074f29b3157928d4b0d
wilsongoltara <wilson.goltara15@gmail.com>
2022-12-30 12:04:26 -0300

    [REFACTOR]: refactor others archives by eslint

--- components/InBuild.tsx
+++ components/InBuild.tsx
@@ -1,7 +1,7 @@
 const InBuild = () => {
   return (
     <article className='flex flex-col justify-center items-center gap-1 h-full w-full text-center text-4xl text-white font-bold'>
-      <h1 className=''>Em construção</h1>
+      <h1>In build</h1>
       <p>
         <span>🚧</span>
         <span>⚠️</span>
