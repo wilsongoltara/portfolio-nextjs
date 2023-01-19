b57831514d15c9546225962052abf52ca6ccf5fd
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-19 12:33:35 -0300

    [REFACTOR]: export projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,23 +1,21 @@
-import indioCoffee from '@assets/indio-coffee.png';
-import notificationsServices from '@assets/notifications-services.png';
-import trybeFutebolClube from '@assets/trybe-futebol-clube.png';
+import * as imageProjects from '@assets/projects';
 import { CardProps } from '@interfaces/props';
 
 export const projects: CardProps[] = [
   {
-    pathProject: notificationsServices,
-    nameProject: 'Notifications Microservices',
-    description: 'A fully scalable and testable notifications microservice',
-    link: 'https://github.com/wilsongoltara/notifications-service',
-  },
-  {
-    pathProject: indioCoffee,
+    pathProject: imageProjects.indioCoffee,
     nameProject: 'Indio Coffee',
     description: 'A simple clone of Starbucks main page',
     link: 'https://indiocoffee.surge.sh/'
   },
   {
-    pathProject: trybeFutebolClube,
+    pathProject: imageProjects.notificationsServices,
+    nameProject: 'Notifications Microservices',
+    description: 'A fully scalable and testable notifications microservice',
+    link: 'https://github.com/wilsongoltara/notifications-service',
+  },
+  {
+    pathProject: imageProjects.trybeFutebolClube,
     nameProject: 'Trybe Futebol Clube',
     description: 'TFC is an informational website about football matches and rankings',
     link: 'https://github.com/wilsongoltara/trybe-futebol-clube'
