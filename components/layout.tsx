1e52331336d8514f9e534175229e9d36f112f4b4
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-21 12:57:18 -0300

    [FIX]: Add meta tags for SEO

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,9 @@
 import { ChildrinProps } from '@interfaces/props';
-import { createContext, useState } from 'react';
-import Content from './global/Content';
+import { createContext, useState, lazy, Suspense } from 'react';
 import Footer from './global/Footer';
 import NavBar from './global/NavBar';
+
+const Content = lazy(() => import('./global/Content'));
 
 export const AppContext = createContext(null);
 
@@ -13,7 +14,9 @@ export default function Layout({ children }: ChildrinProps) {
     <AppContext.Provider value={{ nav, setNav }}>
       <main className='flex flex-col justify-between items-center min-h-screen'>
         <NavBar />
-        <Content>{children}</Content>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Content>{children}</Content>
+        </Suspense>
         <Footer />
       </main>
     </AppContext.Provider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,14 @@ export default function Home() {
           content='Full-stack developer, JavaScript enthusiast, and course creator'
           key='desc'
         />
+        <meta name="keywords" content="Wilson, Goltara, Full Stack, Developer, Portfolio, Web developer" />
         <meta property='og:type' content='website' />
+        <meta property='og:image' content='/assets/meta.jpg' />
+        <meta property='og:title' content='Wilson Goltara' />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <link rel="apple-touch-icon" href="/assets/me.png"></link>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Hero prefix={prefix} name={name} describe={describe} />
