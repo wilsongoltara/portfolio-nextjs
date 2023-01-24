28167b8e6e2f80fff602ab7c5210861252d2fe06
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-01-24 17:11:58 -0300

    [REFACTOR]: Logic Projects

--- src/infra/lib/projects.ts
+++ src/infra/lib/projects.ts
@@ -3,19 +3,19 @@ import {CardProps} from '@interfaces/interfaceProps';
 
 export const projects: CardProps[] = [
   {
-    pathProject: imageProjects.indioCoffee,
+    pathImage: imageProjects.indioCoffee,
     nameProject: 'Indio Coffee',
     description: 'A simple clone of Starbucks main page',
     link: 'https://indiocoffee.surge.sh/'
   },
   {
-    pathProject: imageProjects.notificationsServices,
+    pathImage: imageProjects.notificationsServices,
     nameProject: 'Notifications Microservices',
     description: 'A fully scalable and testable notifications microservice',
     link: 'https://github.com/wilsongoltara/notifications-service',
   },
   {
-    pathProject: imageProjects.trybeFutebolClube,
+    pathImage: imageProjects.trybeFutebolClube,
     nameProject: 'Trybe Futebol Clube',
     description: 'TFC is an informational website about football matches and rankings',
     link: 'https://github.com/wilsongoltara/trybe-futebol-clube'
