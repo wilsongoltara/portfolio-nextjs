0e3c8eac1138ed33551f58b445c698425b80754f
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-01-24 02:25:04 -0300

    [REFACTOR]: Update Head.tsx

--- src/infra/components/Head.tsx
+++ src/infra/components/Head.tsx
@@ -10,13 +10,13 @@ export default function Head({title}: HeadProps) {
         content="Full-stack developer, JavaScript enthusiast, and course creator"
         key="desc"
       />
-      <meta name="keywords" content="Wilson, Goltara, Full Stack, Developer, Portfolio, Web developer"/>
+      <meta name="keywords" content="Wilson, Goltara, Wilson Goltara, Full Stack, Developer, Portfolio, Web developer"/>
       <meta property="og:type" content="website"/>
-      <meta property="og:image" content="/meta.jpg"/>
+      <meta property="og:image" content="/public/meta.jpg"/>
       <meta property="og:title" content="Wilson Goltara"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta name="googlebot" content="index, follow"/>
-      <link rel="icon" href="/favicon.ico"/>
+      <link rel="icon" href="/public/favicon.ico"/>
     </NextHead>
   );
 }
