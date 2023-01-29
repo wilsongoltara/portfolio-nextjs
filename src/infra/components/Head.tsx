cbd66100c8306e1e8900f7c874a7988fd217b9a8
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-01-29 17:58:53 -0300

    refactor meta tags

--- src/infra/components/Head.tsx
+++ src/infra/components/Head.tsx
@@ -5,18 +5,22 @@ export default function Head({children}: ChildrenProps) {
   return (
     <NextHead>
       <title>{children}</title>
+      <meta charSet='utf-8' />
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link rel="icon" href="/favicon.ico"/>
+      <meta property="og:title" content="Wilson Goltara"/>
       <meta
         name="description"
         content="Full-stack developer, JavaScript enthusiast, and course creator"
         key="desc"
       />
       <meta name="keywords" content="Wilson, Goltara, Wilson Goltara, Full Stack, Developer, Portfolio, Web developer"/>
+      <meta property='og:site_name' content='Wilson Goltara Portfolio' />
       <meta property="og:type" content="website"/>
+      <meta property="og:theme_color" content="dark"/>
+      <meta property='og:url' content='https://wilsongoltara.me' />
       <meta property="og:image" content="/meta.webp"/>
-      <meta property="og:title" content="Wilson Goltara"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta name="googlebot" content="index, follow"/>
-      <link rel="icon" href="/favicon.ico"/>
+      <meta name="robots" content="index, follow"/>
     </NextHead>
   );
 }
