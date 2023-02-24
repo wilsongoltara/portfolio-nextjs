a86125482f8af58d2154fe9d1b5ce5b46cee5304
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-02-24 17:47:36 -0300

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import Layout from '@infra/components/Layout';
 import AppContextProvider from '@infra/context/AppContext';
+import { Analytics } from '@vercel/analytics/react';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
@@ -10,6 +12,7 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
       <Layout>
         <AnimatePresence mode={'wait'} >
           <Component key={router.pathname} {...pageProps} />
+          <Analytics />
         </AnimatePresence>
       </Layout>
     </AppContextProvider>
